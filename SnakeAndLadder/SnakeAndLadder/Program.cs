f6a153e50a34a3ac486aa7c96e60072921e598a6
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-04 21:54:33 -0600

    checking player position

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -12,6 +12,20 @@ namespace SnakeAndLadder
             Random rand = new Random();
             int diceNum = rand.Next(1, 7);
             Console.WriteLine("The random dice number is :" + diceNum);
+            position = rand.Next(0, 3);
+            Console.WriteLine("The position of the player" + position);
+            if (position == 1)
+            {
+                Console.WriteLine("Ladder moves ahead by the position received in the die");
+            }
+            else if (position == 2)
+            {
+                Console.WriteLine("Snake moves behind by the position received in the die");
+            }
+            else
+            {
+                Console.WriteLine("The player stays in same position");
+            }
         }
     }
 }
