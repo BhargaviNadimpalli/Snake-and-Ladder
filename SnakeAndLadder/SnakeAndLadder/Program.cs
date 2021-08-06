e6e934916dc737104760f928dd5e8df225098e5a
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-05 22:59:43 -0600

    refactor:playing with two players

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -7,41 +7,59 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Snake and ladder game");
-            int player = 1;
+         
             int position;
-            int position1 = 0;
-            while (position1 < 100)
+            int player = 0;
+            int[] playerPosition = new int[2] { 0, 0 };
+            int diceRollCount = 0;
+            while (playerPosition[player] < 100)
             {
                 Random rand = new Random();
                 int diceNum = rand.Next(1, 7);
                 Console.WriteLine("The random dice number is :" + diceNum);
+                diceRollCount++;
                 position = rand.Next(0, 3);
                 Console.WriteLine("The position of the player" + position);
                 if (position == 1)
                 {
-                    if (position1 <= 100)
+                    if (playerPosition[player] + diceNum <= 100)
                     {
-                        position1 = position1 + diceNum;
+                        playerPosition[player] = playerPosition[player] + diceNum;
                     }
-                    Console.WriteLine("position of die :" + position1);
+                    Console.WriteLine(" player " + player + "position: " + playerPosition[player]);
                 }
                 else if (position == 2)
                 {
-                    position1 = position1 - diceNum;
-                    Console.WriteLine("position of die :" + position1);
-                    if (position1 < 0)
+                    playerPosition[player] = playerPosition[player] - diceNum;
+                    if (playerPosition[player] < 0)
                     {
-                        position1 = 0;
+                        playerPosition[player] = 0;
                     }
+
+                    Console.WriteLine(" player " + player + "position: " + playerPosition[player]);
                 }
                 else
                 {
-                    position1 = position1 + 0;
-                    Console.WriteLine("position of die :" + position1);
+                    Console.WriteLine(" player " + player + "position: " + playerPosition[player]);
                 }
-            Console.WriteLine("The player wins with the position" + position1);
+                 if (position == 0 || position == 2)
+                {
+                    if (player == 0)
+                    {
+                        player = 1;
+                    }
+                    else
+                    {
+                        player = 0;
+                    }
+                }
+                }
+            Console.WriteLine("The winner of snake and ladder is player" + player);
+                    Console.WriteLine("The number of times dice roll is :" + diceRollCount);
+               /// Console.WriteLine("The winning player is :" + winningPlayer);
+              
             }
 
         }
     }
-}
+
